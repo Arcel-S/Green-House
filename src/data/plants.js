e682c4f229853c178824a030d2a19a9822ffc750
Arcel-S <marcellsiregar1204@gmail.com>
2026-03-19 18:36:48 +0700

    feat: Add plant catalog data with categories and care difficulty

---
+++ src/data/plants.js
@@ -0,0 +1,150 @@
+export const featuredPlants = [
+  {
+    id: 1,
+    name: 'Monstera Adansonii',
+    slug: 'monstera-adansonii',
+    difficulty: 'Mudah',
+    difficultyColor: 'green',
+    price: 'Rp 85.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAvWtB52KJqqIjJVrdY5W0ME0QBH76Yt3GOm9h1aNrSFCLUxTxaghPtqdiLQc6NlJNmAKT2u7XOfq1MKR5V_mruA6VO-xoeh_Le9poSd69eIIMEZEtxnpziwpOeXoEwKviwZH4lnvB882fRVpEM-aQmqrqSDPHmiWp3Wij3v56s329J730cybSXQRoTFU6h6YSWRPh1i7pHkbJscvDjYPS7QS8YpHtORySnzaK5fVg3m-zGE2lx2SGSSiuxEd2Ffqh7QvAyZ3r46Q',
+  },
+  {
+    id: 2,
+    name: 'Snake Plant',
+    slug: 'snake-plant',
+    difficulty: 'Sangat Mudah',
+    difficultyColor: 'green',
+    price: 'Rp 45.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAnMf1G5GTCizcW4RhkbgjblQYYcR00zRFgSm8KrFHHX2gTlxoc_xsdHfRJJXlrLBphOC0qx0VUulO3bbSmGvCzuMSZLgxUc1YZQzYbQeNWtR912gKZ9bWVUHQ4IjkqBzIe6I6cqSBPIqdo5mqpxSdnVQEvHCKQ3XWwgAe7iecQkZzZzb0ikBH1uOeZPaYIEDz3JAzkOXVzVu5p18usKSlPIhL9WqDYz3th4LB-Ec9lmSvG_ac3IK25lnqwB8_R4mDs6uId4uUgOw',
+  },
+  {
+    id: 3,
+    name: 'Calathea Orbifolia',
+    slug: 'calathea-orbifolia',
+    difficulty: 'Sedang',
+    difficultyColor: 'orange',
+    price: 'Rp 120.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuBiyuwGvnfODZDpKJfrHTN2AoOAZ5jm7WsP-GCYLYhr_dMq_ggMvukXjvyG5tpm5WScYojYUUEQHnb9nVwbemz7eIUUBrU_uTKTMg6NtQsC1ABKlt9kZILL0Mm01ftYliEEQy0ArgVoS3D1R8nkGssco-KNqA-e07L2uCQu5kOcGphRFMnysUtxJaWP2wFiwwxC74nUF12pvZt8-ULSPBUXdJawLmsQTjVV6cdmD4relNcQ5EQIMcVrJo1QEY3dSeYL45_JT1AWcA',
+  },
+  {
+    id: 4,
+    name: 'Ficus Lyrata',
+    slug: 'ficus-lyrata',
+    difficulty: 'Mudah',
+    difficultyColor: 'green',
+    price: 'Rp 150.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCV8xR2jwfNerFwBJ2dFj3ospazHHEYWEvj-_-wz7Hl8sqBwb2aIZ6IPtGFrBj0aStM5Qvt5JNcv2i3hksjo93fj-QP3BCm9Omti_kvlIVaf41lsFX1p3ApxmtEJqwjlNA3DpweYLh1mkMaaIn_eJRuNVh_7EbXDzJYPybf7-eGI8cgO0N69s4UVeraXSvOC9CqJrYogBmNOZeDsCbokLjnOYTNBDVcu8Ovay1HamfeEPKh5WaCHEdCbxcILxCKTojo6czOtL_-SQ',
+  },
+]
+
+export const catalogPlants = [
+  {
+    id: 'catalog-monstera-deliciosa',
+    slug: 'monstera-deliciosa',
+    name: 'Monstera Deliciosa',
+    category: 'Indoor',
+    careDifficulty: 'Mudah',
+    price: 'Rp 125.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAU5IVur80sNyzXKlGKSBB62IiHBxUqQpSr4YvtQ2BrLUbboHT2yzv0RfRy7T4J1fTf72NaS60kYU7YwrBT5_ir0UVlYrZDRVl_qWQxbxtAX008wSKhWb5AC5Xh-KMQiC11796aJ02fn_lm2Hhi_05EmMKukROZGjfCqkJL6mfsPEEZp1PVLtbUDV90R44Uagm-wMR0VcuHf9crXewCDAXTlN0Xzvu9RmxfbRwYUOvxaZ-2mfSmCXfy1FKWFMAxF5a-xKDil6Zp8w',
+    alt: 'Monstera Deliciosa plant in a minimalist white pot',
+    favorite: true,
+    inStock: true,
+  },
+  {
+    id: 'catalog-sansevieria-trifasciata',
+    slug: 'sansevieria-trifasciata',
+    name: 'Sansevieria Trifasciata',
+    category: 'Indoor',
+    careDifficulty: 'Sangat Mudah',
+    price: 'Rp 85.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuB3UBmx6ABe62td5u0v0Tj2AMFN7F4bqEtgBOVLgQnm-NtBK1T6YD2sMK8WOwLZ5jk6iV-nqFY63NXt6QSqGoWaO7z8zQYsvqgqaZTkH8XHAeShKh96wZwfOFzbbaxqtjoNSi9SXFZYzYhPz97xp6j-rkrsHThNHU2OuSkSIRFQwe7B8qf8Hes-zbfjpXZdmML06LP3lJZowQsdrgn5NWuqYuRtyKv50mKUg_3Z5Gei4SmvCqHrYulNBuqhanULZZ_UWiE-b3EsrQ',
+    alt: 'Sansevieria Snake Plant in a stylish pot',
+    favorite: false,
+    inStock: true,
+  },
+  {
+    id: 'catalog-calathea-orbifolia',
+    slug: 'calathea-orbifolia',
+    name: 'Calathea Orbifolia',
+    category: 'Indoor',
+    careDifficulty: 'Sedang',
+    price: 'Rp 150.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuB_A2CDfA7av6t_UozuhMdoj2yh5H2VFAx_94nzYTejWdNuEHOndu2jgndImtve153POIgK2pdwOX2zfFIciDsQ8QHTU3ratreG8nJw1Jve_qk4XfUFkxi_zubbt6TyoNl14iY3lDDCw0EF8LI7jQjlzhSNw5y9EnH1FyuE6R3A9_aPupKogOIlVHkST2hNXCtgKX33um-ALCrs1FKzLmRSw7Gf9etT58543XDO3duZm7WPMvOjkl7c7EYZ15tOPnEXZXXY2cQukQ',
+    alt: 'Calathea Orbifolia showing beautiful leaf patterns',
+    favorite: false,
+    inStock: true,
+  },
+  {
+    id: 'catalog-echeveria-agavoides',
+    slug: 'echeveria-agavoides',
+    name: 'Echeveria Agavoides',
+    category: 'Sukulen',
+    careDifficulty: 'Sangat Mudah',
+    price: 'Rp 45.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuDjAs2CaewkiZz67lyM6GJbl_Pvo40F97BQFebTP4uSM3zUv_R7xsU5-dFBSxS8Z6X9ePBUFlHjUygdZbHODhjgqjkW2_8NOap8hFffRBfdLQWBVpVymdHwSxNKba8CEBBSOVjczCs4AcbvjonlgyHmyEeGmz9LetOqf6nOthqlpBbUINCOkDKHg7PMlA-vv0p1RFUbs69r9LEvNy1c-K0HaLOGvhbWgufLaFXhjwmYwzuqFpbiDBaIB5k6XYaogI4gy2FJZuokDA',
+    alt: 'Assorted mini succulents in decorative bowls',
+    favorite: false,
+    inStock: true,
+  },
+  {
+    id: 'catalog-fiddle-leaf-fig',
+    slug: 'fiddle-leaf-fig',
+    name: 'Fiddle Leaf Fig',
+    category: 'Tanaman Gantung',
+    careDifficulty: 'Sedang',
+    price: 'Rp 320.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCJxWCe6kN3KAY3gv4qynWIVcS8kAl6KCcd5fN51axsuxMgi2U4xReXkOob8AIYksN6A7eF8snQwdOvsMSJ7MxAdvgFQEqeKCUkP5C5e7cgHVT4AdWy8ymApThPvKPBxDAgMccXsgk4ll8bj2eW3vPfCFv6AH8fiEHcmycJ9OA39ZpxYweYhAN7JgQtLA9Bg7nWo8njbfe0iogxEti8Cp_PMpOtvzibRDX8NKeNpdEiGrx5HZnCoHkOmO7xeeJpvwThaYm5qu2GsA',
+    alt: 'Large Fiddle Leaf Fig tree in a basket',
+    favorite: false,
+    inStock: true,
+  },
+  {
+    id: 'catalog-aloe-vera-barbadensis',
+    slug: 'aloe-vera-barbadensis',
+    name: 'Aloe Vera Barbadensis',
+    category: 'Meja Kerja',
+    careDifficulty: 'Mudah',
+    price: 'Rp 55.000',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCg65niduCzdrqIuMd4LD33mIjwCBGjTQVBxGqwfKxu1qVinKeQNhljJBJfd7nVTEg-tLIHmh5XvvkzoiozobcvVaR6zUvMe9kco4XLk6oZ-pk2_LY9WdBj8ayLwOjXjyYOjq1UGdKkkINKazaWoR23Ikpgm-dTy5nSEtghRvfSP9oBBznrOzriiFMrO6Eie5gHI5RZm8jM830zZhymAuxz82O1okE56ZwREQJ-PnMDTN6WnU4B0PHZynzhBHNP0r40CYWXRbkcsA',
+    alt: 'Healthy Aloe Vera plant closeup',
+    favorite: false,
+    inStock: true,
+  },
+]
+
+export const plantDetailsBySlug = {
+  'monstera-deliciosa': {
+    slug: 'monstera-deliciosa',
+    name: 'Monstera Deliciosa',
+    price: 'Rp 150.000',
+    availabilityLabel: 'Tersedia',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuACi5iZ5_qmjm0coNU3vGEpNp4ipK1HaaOJ0Cmw7Z-zWaH1TQONn02CPQCyOW8AnskOwy47atrbHy1oTUfK5MMMUdRN-hAN4FwYsEexTo93YvcoQg2mZqtgWaj2TT8_V-gMVaj9P1T9Y7nuBWAV8wQIL8QD4IUZPBGpuTUM38XgI40fv0vlpZZ0_5BdubL1g5Jx22Lssk6tQ2FMwRhqqlomhikPfWDFz3UintQ6li7547HgQXdIyadzwmU-kH0bpNt3LiE7UmxC3A',
+    imageAlt: 'Close up photo of a lush Monstera Deliciosa plant leaf',
+    highlights: [
+      { icon: 'light_mode', label: 'Cahaya', value: 'Menengah' },
+      { icon: 'water_drop', label: 'Penyiraman', value: '1-2x/Minggu' },
+      { icon: 'fitness_center', label: 'Kesulitan', value: 'Pemula' },
+    ],
+    description:
+      'Monstera Deliciosa, atau yang dikenal sebagai Swiss Cheese Plant, adalah tanaman indoor populer yang dikenal dengan lubang-lubang unik pada daunnya yang lebar dan hijau mengkilap. Cocok untuk menambah kesan tropis di rumah Anda.',
+    careSteps: [
+      'Siram tanaman saat 2-3 cm lapisan tanah bagian atas sudah terasa kering.',
+      'Bersihkan daun secara berkala dengan kain lembab agar proses fotosintesis lancar.',
+      'Berikan pupuk cair setiap bulan selama musim pertumbuhan.',
+    ],
+    lightNeeds:
+      'Menyukai cahaya terang yang tidak langsung. Paparan sinar matahari langsung yang terlalu kuat dapat membakar daun, sementara cahaya yang terlalu sedikit akan memperlambat pertumbuhan dan membuat daun tidak berlubang.',
+    whatsappUrl: 'https://wa.me/6281234567890',
+  },
+}
